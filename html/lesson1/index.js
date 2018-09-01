41404174f892728ba58d27bbe61147e5a7f033f4
mich71095 <ricardo.embenitez@gmail.com>
2018-09-01 18:14:27 +0800

    exercise 5: Multiple colors

--- html/lesson1/index.js
+++ html/lesson1/index.js
@@ -1,10 +1,14 @@
-var red = [0, 100, 63];
-var orange = [40, 100, 60];
-var green = [75, 100, 40];
-var blue = [196, 77, 55];
-var purple = [280, 50, 60];
+// Color variables:
+red = [0, 5, 63];
+orange = [40, 100, 60];
+green = [75, 100, 40];
+blue = [196, 77, 55];
+purple = [280, 50, 60];
 
-var message = 'Change the message!';
+// Letters in the message will cycle through these colors:
+letterColors = [red, orange, green, blue, purple];
 
-drawName(message, blue);
+message = 'Multiple colors!';
+
+drawName(message, letterColors);
 bounceBubbles();
