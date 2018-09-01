6fb04cad70ed7e9ecb6fa55e61943c15e4d4507d
mich71095 <ricardo.embenitez@gmail.com>
2018-09-01 18:16:50 +0800

    exercise 6: Change physics

--- html/lesson1/index.js
+++ html/lesson1/index.js
@@ -1,14 +1,21 @@
-// Color variables:
-red = [0, 5, 63];
+// Define color variables:
+red = [0, 100, 63];
 orange = [40, 100, 60];
 green = [75, 100, 40];
 blue = [196, 77, 55];
 purple = [280, 50, 60];
-
-// Letters in the message will cycle through these colors:
 letterColors = [red, orange, green, blue, purple];
 
-message = 'Multiple colors!';
+// This variable controls the smallest distance at which a mouse will 
+mouseResponseThreshold = 100;
+
+// This variable controls how strongly the dots will try to return to their starting position
+friction = 0.8;
+
+// This variable controls how much the dots will rotate when interacting
+rotationForce = 0.1;
+
+message = 'Change the physics!';
 
 drawName(message, letterColors);
 bounceBubbles();
