21243a222469267037259605d9579035d8fc8dcb
mich71095 <ricardo.embenitez@gmail.com>
2018-09-01 18:13:29 +0800

    exercise 4: Changing colors

--- html/lesson1/index.js
+++ html/lesson1/index.js
@@ -1,5 +1,10 @@
-// Change the value of the message variable to change the text on screen
-message = 'new message';
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
 
-drawName(message);
+var message = 'Change the message!';
+
+drawName(message, blue);
 bounceBubbles();
